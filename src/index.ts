5a3bac12ad9225607a25a88101d6b566fb25bb3a
ntwigs <oscar.cv.nordquist@gmail.com>
2022-09-15 20:02:55 +0200

    feat: handle script flow

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import { appendButtons } from './main/button/append-buttons'
+import { getButtons } from './main/button/get-buttons'
+import { getCopyButton } from './main/button/get-copy-button'
+import { logger } from './utils/logger'
+import { withError } from './utils/with-error'
+
+const main = () => {
+  const [button, buttonError] = withError<Element>(() => getCopyButton())
+  if (buttonError) {
+    return logger(buttonError)
+  }
+
+  const [buttons, buttonsError] = withError<Element[]>(() => getButtons(button))
+  if (buttonsError) {
+    return logger(buttonsError)
+  }
+
+  appendButtons(button, buttons)
+}
+
+main()
