656a12bc0095c887634f34a62466ff52f842ef88
ntwigs <oscar.cv.nordquist@gmail.com>
2022-09-19 17:45:34 +0200

    refactor: set title as HTMLHeadingElement

--- src/main/title/get-dev-title.ts
+++ src/main/title/get-dev-title.ts
@@ -1,5 +1,5 @@
 export const getDevTitle = (element: Element): Element => {
-  const titleCopy = element.cloneNode()
+  const titleCopy = element.cloneNode() as HTMLHeadingElement
 
   titleCopy.textContent = 'Dev Install'
 
