269b8f22298b6f4c49bddf8f95b88d2ca34504ab
ntwigs <oscar.cv.nordquist@gmail.com>
2022-09-19 17:46:24 +0200

    feat: set existing button as div element

--- src/main/button/get-copy-button.ts
+++ src/main/button/get-copy-button.ts
@@ -2,7 +2,7 @@ const buttonSelector =
   '.lh-copy.truncate.ph0.mb3.black-80.b5be2af6.f6.flex.flex-row'
 
 export const getCopyButton = (): Element => {
-  const button = document.querySelector(buttonSelector)
+  const button = document.querySelector<HTMLDivElement>(buttonSelector)
 
   if (!button) {
     throw new Error('Could not find button')
