1e4633cc3b957095303236713074c61911e2958a
ntwigs <oscar.cv.nordquist@gmail.com>
2022-09-19 17:46:02 +0200

    feat: set existing title as heading element

--- src/main/title/get-copy-title.ts
+++ src/main/title/get-copy-title.ts
@@ -1,7 +1,7 @@
 const titleSelector =
   '#top > div.fdbf4038.w-third-l.mt3.w-100.ph3.ph4-m.pv3.pv0-l > h3'
 export const getCopyTitle = () => {
-  const title = document.querySelector(titleSelector)
+  const title = document.querySelector<HTMLHeadingElement>(titleSelector)
 
   if (!title) {
     throw new Error('Could not find title')
