3a2e9e04db063696ee4e2853a9a9e45d47525b79
ntwigs <oscar.cv.nordquist@gmail.com>
2022-09-23 12:16:40 +0200

    feat: get button without clone

--- src/main/button/get-existing-button.ts
+++ src/main/button/get-existing-button.ts
@@ -1,12 +1,12 @@
 const buttonSelector =
   '.lh-copy.truncate.ph0.mb3.black-80.b5be2af6.f6.flex.flex-row'
 
-export const getCopyButton = (): HTMLDivElement => {
+export const getExistingButton = (): HTMLDivElement => {
   const button = document.querySelector<HTMLDivElement>(buttonSelector)
 
   if (!button) {
     throw new Error('Could not find button')
   }
 
-  return button.cloneNode(true) as HTMLDivElement
+  return button
 }
