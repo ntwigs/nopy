f97931c438496ed0ace7b0b7cd2cb99a4e8236db
ntwigs <oscar.cv.nordquist@gmail.com>
2025-11-30 10:32:00 +0100

    fix: update tab change info type

--- src/main/serviceworkers/on-update.ts
+++ src/main/serviceworkers/on-update.ts
@@ -2,7 +2,7 @@ import { withPackageCheck } from '../../utils/with-package-check'
 import { PATH_CHANGE } from '../events/path-change'
 
 const isPathChange = (
-  changeInfo: chrome.tabs.TabChangeInfo,
+  changeInfo: chrome.tabs.OnUpdatedInfo,
   tab: chrome.tabs.Tab
 ) => {
   return changeInfo.status && changeInfo.status == 'complete' && tab.url
